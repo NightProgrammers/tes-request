ba031ceae9729393795b60f4858b01b83297c337
Wuhui Zuo <wuhuizuo@126.com>
2017-05-26 15:34:33 +0800

    add bin `tes-client`: support request_res|release_res|request_pool|release_pool

--- lib/tes/request/client.rb
+++ lib/tes/request/client.rb
@@ -1,4 +1,6 @@
 require 'json'
+require 'httpclient'
+require 'timeout'
 
 module Tes
   module Request
@@ -69,6 +71,109 @@ module Tes
           end
           res_body
         end
+      end
+    end
+
+    class ClientBin
+      def self.exit_usage(program, exit_code=2)
+        puts <<EOF
+Usage:
+    % #{program} {TesWebUrl} {User} request_res  {ResourceId}  [1|0]                       # Request Specified Resource
+    % #{program} {TesWebUrl} {User} release_res  {ResourceId}                              # Release Specified Resource
+    % #{program} {TesWebUrl} {User} request_pool {PoolAskFile} {SaveFile} [TimeoutSeconds] # Request Env Pool 
+    % #{program} {TesWebUrl} {User} release_pool [PoolFile]                                # Release Env Pool
+EOF
+        exit exit_code
+      end
+
+      def initialize(tes_url, user)
+        driver = HTTPClient.new(base_url: tes_url)
+        driver.ssl_config.verify_mode= OpenSSL::SSL::VERIFY_NONE
+        @client = Client.new(driver)
+        @user = user
+      end
+
+      # Cli runner method
+      # @param [String] op_type
+      # @param [Array<String>] op_args
+      def run(op_type, op_args, program = __FILE__)
+        assert_args(op_type, op_args) ? send(op_type, *op_args) : self.exit_usage(program, 2)
+      end
+
+      private
+
+      def assert_args(op_type, op_args)
+        case op_type
+          when 'request_res'
+            op_args.size.between?(1, 2)
+          when 'release_res'
+            op_args.size == 1
+          when 'request_pool'
+            op_args.size.between?(2, 3)
+          when 'release_pool'
+            op_args.size.between?(0, 1)
+          else
+            false
+        end
+      end
+
+      def request_res(res_id, lock=0)
+        ret = @client.request_res(res_id, @user, lock)
+
+        msg_suffix = "Request resource:(id: #{res_id}, user: #{@user}, lock: #{lock})."
+        ret[:success] ? puts("[ OK ] #{msg_suffix}") : warn("[Fail] #{msg_suffix}")
+
+        ret[:success]
+      end
+
+      def release_res(res_id)
+        ret = @client.release_res(res_id, @user)
+
+        msg_suffix = "Release resource:(id: #{res_id}, user: #{@user})."
+        ret[:success] ? puts("[ OK ] #{msg_suffix}") : warn("[Fail] #{msg_suffix}")
+
+        ret[:success]
+      end
+
+      def release_pool(saved_env_file=nil)
+        if saved_env_file and File.exists?(saved_env_file)
+          env_pool_info = YAML.load_file(saved_env_file)
+          locks = env_pool_info[:lockes]
+          locks.each { |lock| @client.release_res(lock, @user) } if locks
+          puts "[Info] Release env done(user: #{@user}, file: #{saved_env_file})."
+        else
+          @client.release_all_res(@user)
+          puts "[Info] Release env done(user: #{@user})."
+        end
+
+        true
+      end
+
+      # request env pool
+      def request_pool(profile_file, save_file, timeout_secs=0)
+        File.exists?(profile_file) || raise(ArgumentError, "[ ER ] File not found:#{profile_file}")
+
+        asks = File.readlines(profile_file).map!(&:strip).compact
+
+        Timeout.timeout(timeout_secs && timeout_secs.to_i) do
+          print '[Info] Request env...'
+
+          ret = {}
+          until ret[:success]
+            print '.'
+            ret = @client.request_env(@user, asks)
+            sleep 5 unless ret[:success]
+          end
+
+          # request successfully
+          puts 'successfully!'
+          File.open(save_file, 'w') do |f|
+            f.write ret[:data].to_yaml
+            puts '[ OK ] Request env done, saved in: ' + save_file
+          end
+        end
+
+        true
       end
     end
   end
--- tes-request.gemspec
+++ tes-request.gemspec
@@ -31,6 +31,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_runtime_dependency 'java-properties', '~> 0.2'
+  spec.add_runtime_dependency 'httpclient', '~> 2.8.3'
   spec.add_development_dependency 'rake', '~> 12.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'bundler', '~> 1.13'
