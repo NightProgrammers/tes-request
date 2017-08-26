22aaca8149e2a56b65634923fed0931f5b959e46
Wuhui Zuo <wuhuizuo@126.com>
2017-08-26 18:16:46 +0800

    [ADD] unit tests in ask_spec.rb

--- spec/tes/request/ask_spec.rb
+++ spec/tes/request/ask_spec.rb
@@ -3,6 +3,15 @@ describe Tes::Request::Ask do
     Tes::Request::Ask.new(str)
   end
 
+  def share_ask(str)
+    ret = ask(str)
+    ret.lock_type = :share
+    ret
+  end
+  def lock_ask(str)
+    ask(str)
+  end
+
   context :<=> do
     it '属性多少差异比较' do
       expect(ask('hello=1')).to be == ask('hello=1')
@@ -10,6 +19,14 @@ describe Tes::Request::Ask do
       expect(ask('hello=1,name=zwh')).to be > ask('hello=1')
       expect(ask('hello=1')).to be <= ask('hello=1,name=zwh,sex=man')
       expect(ask('hello=1')).to be <= ask('hello=1')
+    end
+    it '锁定资源的方式' do
+      expect(lock_ask('hello=1')).to be > share_ask('hello=1')
+      expect(lock_ask('hello=1')).to be >= share_ask('hello=1')
+      expect(share_ask('hello=1')).to be < lock_ask('hello=1')
+      expect(share_ask('hello=1')).to be <= lock_ask('hello=1')
+      expect(share_ask('hello<2')).to be <= lock_ask('hello<1')
+      expect(lock_ask('hello>2')).to be >= share_ask('hello>1')
     end
 
     it '相似表达式的比较' do
@@ -28,11 +45,19 @@ describe Tes::Request::Ask do
       expect(ask('hello=1,vs_enable=1') <=> ask('hello=2')).to be_nil
       expect(ask('hello>2,vs_enable=1') <=> ask('hello=2')).to be_nil
       expect(ask('hello=2,vs_enable=1') <=> ask('hello=2,vs_enable=0')).to be_nil
+      expect(share_ask('hello>2') <=> lock_ask('hello>1')).to be_nil
     end
   end
   context :merge_able? do
     it 'false' do
       expect(ask('hello=2,vs_enable=1')).not_to be_merge_able ask('hello=2,vs_enable=0')
+      expect(lock_ask('hello=2,vs_enable=1')).not_to be_merge_able share_ask('hello=2,vs_enable=0')
+    end
+    it 'true' do
+      expect(ask('hello=2,vs_enable=1')).to be_merge_able ask('hello=2,vs_enable>=0')
+      expect(share_ask('hello>=1,vs_enable=1')).to be_merge_able lock_ask('hello=2,vs_enable>=0')
+      expect(ask('hello>=1,vs_enable<=1')).to be < ask('hello=2,vs_enable=0')
+      expect(ask('hello>=1,vs_enable<=1')).to be_merge_able ask('hello=2,vs_enable=0')
     end
   end
 end
